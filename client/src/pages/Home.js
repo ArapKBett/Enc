deb87e639c19ef9816641154ff92aa70f9f5e094
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-15 12:54:59 +0300

    Create Home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Typography, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import EntryList from '../components/EntryList';
+import EntryForm from '../components/EntryForm';
+
+const Home = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return (
+    <Container>
+      <Typography variant="h3" gutterBottom>Encyclopedia</Typography>
+      {user.role === 'admin' && (
+        <>
+          <Button component={Link} to="/admin" variant="contained" color="primary" style={{ marginBottom: '20px' }}>
+            Manage Categories/Users
+          </Button>
+          <EntryForm onSubmit={() => window.location.reload()} />
+        </>
+      )}
+      <EntryList />
+    </Container>
+  );
+};
+
+export default Home;
